d20ac4f2769576b625c0851df7dd9adadfa0c0d0
SohanSonu303 <87355196+SohanSonu303@users.noreply.github.com>
2024-07-10 21:23:10 +0530

    test changes

---
+++ reactsample/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Boy from './Boy.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Boy></Boy>
+    </div>
+  );
+}
+
+export default App;
